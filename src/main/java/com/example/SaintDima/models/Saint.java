0aa8a0f18016d9fa33fdcfeaf7bdebc7c4e1bb8b
BaldaAzz <kill200419@gmail.com>
2024-10-18 09:56:13 +0300

    Create Saint model

---
+++ src/main/java/com/example/SaintDima/models/Saint.java
@@ -0,0 +1,29 @@
+package com.example.SaintDima.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Saint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+    private String fathersName;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfDeath;
+    private String picturePath;
+
+//    добавить таблицу со списком мест
+//    private List placesOfWorship;
+
+//    добавить таблицу с событиями
+//    private List significantEvents;
+}
