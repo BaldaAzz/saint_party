e4532073c80fc853c236c54dce67f06500045a6b
BaldaAzz <kill200419@gmail.com>
2024-10-18 09:09:35 +0300

    Create project

---
+++ src/main/java/com/example/SaintDima/SaintDimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SaintDima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintDimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintDimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SaintDima/SaintDimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SaintDima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintDimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
