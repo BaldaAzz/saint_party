bf61b2b49832767c23d5b969482fc0ad23bc3abd
BaldaAzz <kill200419@gmail.com>
2024-10-20 17:58:39 +0300

    Update Saint model

--- src/main/java/com/example/SaintDima/models/Saint.java
+++ src/main/java/com/example/SaintDima/models/Saint.java
@@ -27,4 +27,82 @@ public class Saint {
 
 //    добавить таблицу с событиями
 //    private List significantEvents;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public void setFathersName(String fathersName) {
+        this.fathersName = fathersName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(LocalDate dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public Saint() {
+    }
+
+    public Saint(String name, String surname, String fathersName, LocalDate dateOfBirth, LocalDate dateOfDeath, String picturePath, String biography) {
+        this.name = name;
+        this.surname = surname;
+        this.fathersName = fathersName;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.picturePath = picturePath;
+        this.biography = biography;
+    }
 }
