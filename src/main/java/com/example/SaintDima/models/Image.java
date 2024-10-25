298ead800748517f3615ae2e1ab3c5b932cbdac9
BaldaAzz <kill200419@gmail.com>
2024-10-25 23:18:54 +0300

    Create Image model

---
+++ src/main/java/com/example/SaintDima/models/Image.java
@@ -0,0 +1,27 @@
+package com.example.SaintDima.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    @Lob
+    private byte[] bytes;
+    @OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private SaintPerson saintPerson;
+}
