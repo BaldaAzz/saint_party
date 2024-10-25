542e31669211ccf38b2a6fce7a50b9c0762991ab
BaldaAzz <kill200419@gmail.com>
2024-10-25 23:15:06 +0300

    Update SaintPerson model

--- src/main/java/com/example/SaintDima/models/SaintPerson.java
+++ src/main/java/com/example/SaintDima/models/SaintPerson.java
@@ -1,15 +1,18 @@
 package com.example.SaintDima.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
-public class Saint {
+@Table(name = "saint_persons")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaintPerson {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,90 +22,16 @@ public class Saint {
     private String fathersName;
     private LocalDate dateOfBirth;
     private LocalDate dateOfDeath;
-    private String picturePath;
     private String biography;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Image image;
 
+    public void addImageToSaintPerson(Image image) {
+        image.setSaintPerson(this);
+    }
 //    добавить таблицу со списком мест
 //    private List placesOfWorship;
 
 //    добавить таблицу с событиями
 //    private List significantEvents;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getFathersName() {
-        return fathersName;
-    }
-
-    public void setFathersName(String fathersName) {
-        this.fathersName = fathersName;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public LocalDate getDateOfDeath() {
-        return dateOfDeath;
-    }
-
-    public void setDateOfDeath(LocalDate dateOfDeath) {
-        this.dateOfDeath = dateOfDeath;
-    }
-
-    public String getPicturePath() {
-        return picturePath;
-    }
-
-    public void setPicturePath(String picturePath) {
-        this.picturePath = picturePath;
-    }
-
-    public String getBiography() {
-        return biography;
-    }
-
-    public void setBiography(String biography) {
-        this.biography = biography;
-    }
-
-    public Saint() {
-    }
-
-    public Saint(String name, String surname, String fathersName, LocalDate dateOfBirth, LocalDate dateOfDeath, String picturePath, String biography) {
-        this.name = name;
-        this.surname = surname;
-        this.fathersName = fathersName;
-        this.dateOfBirth = dateOfBirth;
-        this.dateOfDeath = dateOfDeath;
-        this.picturePath = picturePath;
-        this.biography = biography;
-    }
 }
