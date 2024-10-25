0f425f38484e012d97a5bdf18a923ac2c7ce334c
BaldaAzz <kill200419@gmail.com>
2024-10-25 23:12:15 +0300

    Rename and delete some files

--- src/main/java/com/example/SaintDima/repositories/SaintRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SaintDima.repositories;
-
-import com.example.SaintDima.models.Saint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SaintRepository extends JpaRepository<Saint, Long> {
-}
