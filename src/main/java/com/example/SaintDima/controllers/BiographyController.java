83ea66aaa30d49e114efe979dbad5e9de9fa451f
BaldaAzz <kill200419@gmail.com>
2024-10-27 23:25:05 +0300

    Update BiographyController

--- src/main/java/com/example/SaintDima/controllers/BiographyController.java
+++ src/main/java/com/example/SaintDima/controllers/BiographyController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,9 +25,6 @@ public class BiographyController {
     @Autowired
     private SaintPersonService saintPersonService;
 
-    private static final String UPLOAD_DIRECTORY = "/images/";
-
-
     @GetMapping("/")
     public String biographyPage(Model model) {
 //        Добавить отображение всех данных из бд
@@ -44,8 +38,10 @@ public class BiographyController {
     }
 
     @GetMapping("/{id}")
-    public String getBiographyById(Model model) {
-        return "";
+    public String getBiographyById(@PathVariable(value = "id") Long id, Model model) {
+        SaintPerson saintPerson = saintPersonService.getByIdSaintBiography(id);
+        model.addAttribute("saintPerson", saintPerson);
+        return "biography";
     }
 
     @PostMapping("/add")
